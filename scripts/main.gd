bc07deffac39a61e063087864fb9b4f1d34e3d3b
Harun1804 <Harun.arrasyid1804@gmail.com>
2025-07-13 11:43:25 +0700

    Add start and end screen

--- scripts/main.gd
+++ scripts/main.gd
@@ -1,10 +1,14 @@
 extends Node2D
 
+@onready var startButton = $CanvasLayer/CenterContainer/Start
+@onready var gameOver = $CanvasLayer/CenterContainer/GameOver
+
 var enemy = preload("res://scenes/enemy.tscn")
 var score = 0
 
 func _ready():
-	spawn_enemies()
+	startButton.show()
+	gameOver.hide()
 
 func spawn_enemies():
 	for x in range(9):
@@ -18,3 +22,22 @@ func spawn_enemies():
 func _on_enemy_died(value):
 	score += value
 	$CanvasLayer/UI.updateScore(score)
+
+func _on_start_pressed() -> void:
+	startButton.hide()
+	newGame()
+
+func newGame():
+	score = 0
+	$CanvasLayer/UI.updateScore(score)
+	$Player.start()
+	$Player.show()
+	spawn_enemies()
+
+
+func _on_player_died() -> void:
+	get_tree().call_group("enemies", "queue_free")
+	gameOver.show()
+	await get_tree().create_timer(2).timeout
+	gameOver.hide()
+	startButton.show()
